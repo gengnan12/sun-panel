42fa64b2d6839d54b3034f1401f9b7fa4fdde42f
gengnan12 <93430640+gengnan12@users.noreply.github.com>
2025-09-15 13:24:28 +0800

    更新 vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,69 +1,31 @@
-import path from 'path'
-import type { PluginOption } from 'vite'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { resolve } from 'path'
 
-function setupPlugins(env: ImportMetaEnv): PluginOption[] {
-  return [
-    vue(),
-    env.VITE_GLOB_APP_PWA === 'true' && VitePWA({
-      injectRegister: 'auto',
-      manifest: {
-        name: 'Sun-Panel',
-        short_name: 'Sun-Panel',
-        icons: [
-          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
-        ],
-      },
-    }),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/svg-icons')],
-      symbolId: '[name]',
-    }),
-  ]
-}
-
-export default defineConfig((env) => {
-  const viteEnv = loadEnv(env.mode, process.cwd()) as unknown as ImportMetaEnv
-
-  return {
-    resolve: {
-      alias: {
-        '@': path.resolve(process.cwd(), 'src'),
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    minify: 'terser', // 明确设置 minify 为 terser
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
       },
     },
-    plugins: setupPlugins(viteEnv),
-    server: {
-      host: '0.0.0.0',
-      port: 1002,
-      open: false,
-      proxy: {
-        '/api': {
-          target: viteEnv.VITE_APP_API_BASE_URL,
-          changeOrigin: true, // 允许跨域
-          rewrite: path => path.replace('/api/', '/api/'),
-        },
-        '/uploads': {
-          target: viteEnv.VITE_APP_API_BASE_URL,
-          changeOrigin: true, // 允许跨域
-          rewrite: path => path.replace('/uploads/', '/uploads/'),
-        },
-      },
-    },
-    build: {
-      reportCompressedSize: false,
-      sourcemap: false,
-      commonjsOptions: {
-        ignoreTryCatch: false,
-      },
-      terserOptions: {
-        compress: {
-          drop_console: true,
-        },
-      },
-    },
-  }
+    // 或者使用 esbuild（推荐）
+    // minify: 'esbuild',
+    // esbuildOptions: {
+    //   minify: true,
+    // },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
 })
